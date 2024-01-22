b50c545e23564fc48ffcd500fd1cab9f5b8c938d
Samitha Wickramasinghe <89331663+samithadev@users.noreply.github.com>
2024-01-22 12:51:58 +0530

    feat: add remotePatterns for next config file to remote images

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -12,6 +12,9 @@ export default function Home() {
 
       <Image src={"https://images.unsplash.com/photo-1682686580036-b5e25932ce9a?q=80&w=1075&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}  alt="remote img" className={styles.img} width={1000} height={1000}/>
 
+      <Image src={"https://images.pexels.com/photos/18509061/pexels-photo-18509061/free-photo-of-woman-holding-lantern-in-the-evening.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"}  alt="remote img" className={styles.img} width={1000} height={1000}/>
+
+
       <p className={styles.p}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel aut provident aliquam at neque animi dolore doloremque ut culpa deserunt nihil odit rem, adipisci numquam fuga reiciendis nesciunt, a cumque.</p>
       <ul>
         <li><Link href="/" className={styles.link}>Home</Link></li>
